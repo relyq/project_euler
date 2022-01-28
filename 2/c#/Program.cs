82813cb6cc57416bf81227b8ca815ba72c375c71
relyq <nicoale_mainenti@hotmail.es>
2022-01-28 07:33:24 -0300

    problem 2 c#

---
+++ 2/c#/Program.cs
@@ -0,0 +1,20 @@
+﻿int limit = 4000000;
+int fibonacci = 1;
+int last = fibonacci;
+int sum = 0;
+decimal golden = 0;
+
+while (fibonacci <= limit)
+{
+  if (fibonacci % 2 == 0)
+  {
+    sum += fibonacci;
+  }
+  fibonacci += last;
+  last = fibonacci - last;
+}
+
+golden = (decimal)fibonacci / last;
+
+Console.WriteLine(sum);
+Console.WriteLine(golden);
