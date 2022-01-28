1eac51b9cf74ecccfbc4dc9682aa557d67a67768
relyq <nicoale_mainenti@hotmail.es>
2022-01-28 07:06:19 -0300

    problem 1 c#

---
+++ 1/c#/Program.cs
@@ -0,0 +1,18 @@
+﻿List<int> multiples = new();
+
+int limit = 1000;
+int x = 3, y = 5;
+
+for (int i = 2; i < limit; i++)
+{
+  if ((i % x == 0) || (i % y == 0))
+  {
+    multiples.Add(i);
+  }
+}
+
+int sum = 0;
+
+multiples.ForEach((int x) => sum += x);
+
+Console.WriteLine(sum);
